19c00fbd5588d214ffc9c8f00c8a59686b7ef70b
FAHAD <eng.bokhari@hotmail.com>
2024-05-01 02:11:10 +1200

    Add files via upload
    Update on this var: 
       - FIX_BUG_2: Fixed the issue with low accuracy during training and predict test https://github.com/AHAD1405/classify_pinguing/issues/10

--- a2Part1.py
+++ a2Part1.py
@@ -20,7 +20,7 @@ def encode_labels(labels):
 
     return label_encoder, integer_encoded, onehot_encoder, onehot_encoded
 
-def accuracy(predictions, actual_onehot_encoded, instance_length):
+def calculate_acc(predictions, actual_onehot_encoded, instance_length):
     test_correct_predictions = 0
     for predicted_label, actual_label in zip(predictions, actual_onehot_encoded):
         if np.array_equal(predicted_label, actual_label):
@@ -28,6 +28,7 @@ def accuracy(predictions, actual_onehot_encoded, instance_length):
 
     test_accuracy = test_correct_predictions / instance_length
     return test_accuracy
+
 
 if __name__ == '__main__':
     data = pd.read_csv('penguins307-train.csv')
@@ -40,7 +41,9 @@ if __name__ == '__main__':
     instances = scaler.fit_transform(instances)
     # We can't use strings as labels directly in the network, so need to do some transformations
     label_encoder, integer_encoded, onehot_encoder, onehot_encoded = encode_labels(labels)
-    # labels = onehot_encoded
+    
+    labels = onehot_encoded
+    
 
     # Parameters. As per the handout.
     n_in = 4
@@ -50,10 +53,12 @@ if __name__ == '__main__':
 
     initial_hidden_layer_weights = np.array([[-0.28, -0.22], [0.08, 0.20], [-0.30, 0.32], [0.10, 0.01]])
     initial_output_layer_weights = np.array([[-0.29, 0.03, 0.21], [0.08, 0.13, -0.36]])
+    initial_hidden_bias = np.array([[-0.02, -0.20]])
+    initial_output_bias = np.array([[-0.33, 0.26, 0.06]])
 
     nn = Neural_Network(n_in, n_hidden, n_out, initial_hidden_layer_weights, initial_output_layer_weights,
-                        learning_rate)
-
+                        learning_rate, initial_hidden_bias, initial_output_bias)
+    
     print('First instance has label {}, which is {} as an integer, and {} as a list of outputs.\n'.format(
         labels[0], integer_encoded[0], onehot_encoded[0]))
 
@@ -75,11 +80,11 @@ if __name__ == '__main__':
     print('Output layer weights:\n', nn.output_layer_weights)
 
     # TODO: Train for 100 epochs, on all instances.
-    nn.train(instances, onehot_encoded, 5)
+    nn.train(instances, onehot_encoded, 100)
+
     print('\nAfter training:')
     print('Hidden layer weights:\n', nn.hidden_layer_weights)
     print('Output layer weights:\n', nn.output_layer_weights)
-
 
     pd_data_ts = pd.read_csv('penguins307-test.csv')
     test_labels = pd_data_ts.iloc[:, -1]
@@ -88,11 +93,10 @@ if __name__ == '__main__':
     test_instances = scaler.transform(test_instances)
 
     # TODO: Compute and print the test accuracy
-    # Perform prediction on test data 
     tes_label_encoder, test_integer_encoded, test_onehot_encoder, test_onehot_encoded = encode_labels(test_labels)
-    predictions = nn.predict(test_instances)
+    test_predict = nn.predict(test_instances)
     
     # Calculate the accuracy
-    predictions_class = onehot_encoder.transform(np.array(predictions).reshape(-1, 1))  # convert interget predicted into one hot code class
-    acc_test = accuracy(predictions_class, test_onehot_encoded , len(test_instances))
-    print("The Accuracy on test data is: {:.2f} %".format(acc_test*100))
+    test_predict_class = onehot_encoder.transform(np.array(test_predict).reshape(-1, 1))  # convert interget predicted into one hot code class
+    test_accuracy = calculate_acc(test_predict_class, test_onehot_encoded , len(test_instances))
+    print("The Accuracy is: {:.2f} %".format(test_accuracy*100))
